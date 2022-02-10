6983113397f41a10e69d4777ecacbad1c9ebab83
Kiran <kiran.degregorio@gmail.com>
2022-02-10 13:50:37 +0000

    🌍 Added the website files

---
+++ config.js
@@ -0,0 +1,9 @@
+export let config = {
+    name: 'John Doe',
+    imageURL: 'profileImage.png',
+    links: [
+        { title: 'Example #1', url: 'https://example.com/' },
+        { title: 'Example #2', url: 'https://example.com/' },
+        { title: 'Example #3', url: 'https://example.com/' },
+    ]
+}
